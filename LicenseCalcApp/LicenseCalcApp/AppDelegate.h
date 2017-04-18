4e47d99b3959779e82a91f6dabf54aafaef24920
edaotech <edaotech@edaotechdeMacBook-Pro.local>
2017-04-18 12:07:09 +0800

    初始化

---
+++ LicenseCalcApp/LicenseCalcApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LicenseCalcApp
+//
+//  Created by edaotech on 2017/4/18.
+//  Copyright © 2017年 edaotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LicenseCalcApp/LicenseCalcApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LicenseCalcApp
+//
+//  Created by edaotech on 2017/4/18.
+//  Copyright © 2017年 edaotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
